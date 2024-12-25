d1f39804d222291502ad4876f5682e8ff2520725
user.email "dhruvknipsss023@gmail.com <dhruvknipsss023@gmail.com>
2024-12-25 20:35:22 +0530

    new commit

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = [
+	{
+		id: 1,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+		image: "https://images.unsplash.com/photo-1637823370891-7dc8a00f64f9?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		price: "35,758",
+	},
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+		price: "82,560",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: "https://plus.unsplash.com/premium_photo-1661964304872-7b715cf38cd1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dg",
+		price: "29,695",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
+		image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+		price: "31,095",
+	},
+	{
+		id: 5,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+		price: "78,595",
+	},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
+		image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+		price: "68,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: "https://plus.unsplash.com/premium_photo-1697729597066-7b3d09b6dab7?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		price: "68,595",
+	},
+];
+
+export default data;
